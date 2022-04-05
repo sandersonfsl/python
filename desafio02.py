47be33f354ae6068294eabb697795b5ea4e44724
sandersonfsl <sandersonfarias@gmail.com>
2022-04-05 11:32:20 -0300

    second challenge first commit

---
+++ desafio02.py
@@ -0,0 +1,6 @@
+day = input('Whats the day of your birth ? ')
+month = input('Whats the month of your birth ? ')
+year = input('Whats the year of your birth ? ')
+
+print('You were born on',month,day,', ',year,', ',
+'correct? ')
