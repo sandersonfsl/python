ee43f588437116c0ba9d959b5cd9f74e1b9a7340
Sanderson Lima <100845104+sandersonfsl@users.noreply.github.com>
2022-04-05 16:28:46 -0300

    Adding int

--- challenge03.py
+++ challenge03.py
@@ -1,6 +1,6 @@
-number1= input('Type the first number : ')
-number2 = input('Type the second number : ')
+number1 = int(input('Type the first number : '))
+number2 = int(input('Type the second number : '))
 
+sum = number1 + number2
 
-
-print('The sum of these two numbers is :')
+print('The sum of these two numbers is :',sum)
