7133599e6558a927ff9a56d8f4d5001d42581e40
sandersonfsl <sandersonfarias@gmail.com>
2022-04-06 12:04:37 -0300

    updating README + updating challenge03 with format()

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 # python
 
 - abr 5, finishing challenge 3 , video 4
-- abr 6, 
+- abr 6, 13:57 9/47 video
--- challenge03.py
+++ challenge03.py
@@ -1,6 +1,17 @@
-number1 = int(input('Type the first number : '))
-number2 = int(input('Type the second number : '))
+#1OPTION
 
+#number1 = int(input('Type the first number : '))
+#number2 = int(input('Type the second number : '))
+
+#sum = number1 + number2
+
+#print('The sum of these two numbers is :',sum)
+
+
+#2OPTION
+
+number1 = int(input('number1 :' ))
+number2 = int(input('number2:'))
 sum = number1 + number2
+print('The sum of these numbers is: {}'.format(sum))
 
-print('The sum of these two numbers is :',sum)
