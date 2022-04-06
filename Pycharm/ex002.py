8de3d4e5312b70574983854d393a24ef70c9ac06
sandersonfsl <sandersonfarias@gmail.com>
2022-04-06 11:48:12 -0300

    ex001 ex002 formatexamples files creation

---
+++ Pycharm/ex002.py
@@ -0,0 +1,12 @@
+#name = input('Hello, whats your name ')
+#print('Welcome, nice to meet you ',name,' !')
+
+
+#or:
+
+name = input('Hello, whats your name ')
+print('Welcome, nice to meet you {}!'.format(name))
+
+
+
+
---
+++ Pycharm/formatexample.py
@@ -0,0 +1,6 @@
+name = input('name ')
+age = input('age ')
+salary = input('salary ')
+print('{} have {} years old and a U$ {} salary'.format(name,age,salary))
+
+
