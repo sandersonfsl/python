a55bb8a3e42c0f4667e50b69374401a6174da1bd
sandersonfsl <sandersonfarias@gmail.com>
2022-04-19 11:39:48 -0300

    challenges 09, 10 and README.md upgrade

--- challenge10.py
+++ challenge10.py
@@ -1,2 +1,2 @@
-m = int(input('how much brazilian Real do you have : '))
-print('you can buy {} dollars'.format(m/4.65))
+m = float(input('how much brazilian Real do you have  R$  '))
+print('{:.2f} R$ can buy {:.2f} US$'.format(m,m/4.67))
