001e6a547b4b43fe671f9ff679b60f65c2bc81b4
Sanderson Lima <sandersonfarias@gmail.com>
2022-04-22 14:37:15 -0300

    challenge 17

---
+++ challenges/challenge17.py
@@ -0,0 +1,5 @@
+import math
+Os = float(input('OS of the right triangle :'))
+As = float(input('AS of the right triangle: '))
+h = math.sqrt(Os**2+As**2)
+print('triangle hypotenuse is: {}'.format(h))
