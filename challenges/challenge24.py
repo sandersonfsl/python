11079f4176f9ae95afaa92188cd26394d5265452
sandersonfsl <sandersonfarias@gmail.com>
2022-05-18 11:12:08 -0300

    updating challenge24

--- challenges/challenge24.py
+++ challenges/challenge24.py
@@ -1,8 +1,12 @@
-c = input('your city name? ')
-a = c.split()
+city = input('your city name? ')
+a = city.split()
 b = 'san' in a[0]
 
 if b == True :
     print('You live in a holy city! ')
 else :
     print('You dont live in a holy city !')
+
+# OR
+# c = str(input('city? ')).strip()
+# print(c[:5].upper() == 'SAN')
