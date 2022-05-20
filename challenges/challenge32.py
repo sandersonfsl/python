d73439e39c02dbfb4047f70f6c2302668f2ad60c
sandersonfsl <sandersonfarias@gmail.com>
2022-05-20 14:54:39 -0300

    challenge32 leap year

--- challenges/challenge32.py
+++ challenges/challenge32.py
@@ -1,14 +1,16 @@
-y = str(input('year : '))
+# To be a leap year, the year number must be divisible by four – except
+# for end-of-century years, which must be divisible by 400. This means
+# that the year 2000 was a leap year, although 1900 was not. 
 
+y = input('Year ? ')
 
-if y[len(y)- 1] == '0' and y[len(y)- 2] == '0' and int(y) % 400 == 0 :
-    print('leap year')
-
-if int(y) % 4 == 0  :
-    print('leap year')
-
+if y[-1] == '0' and y[-2] == '0':
+    d = int(y) % 400
+    if d == 0:
+        print('Leap year')
+    else :
+        print('Common year')
+elif int(y) % 4 == 0:
+    print('Leap year')
 else :
-    print('common year')
-
-
-## 2100 is a common year
+    print('Common year')
