dcfbe9ae5b8155c6512f4943ac65c4ce15edd820
sandersonfsl <sandersonfarias@gmail.com>
2022-05-31 17:59:10 -0300

    challenge39 adjust

--- challenges/challenge39.py
+++ challenges/challenge39.py
@@ -3,10 +3,13 @@ y = int(input('Your year of birth : '))
 cy = date.today().year
 
 if cy - y < 18 :
-    print('youll still enlist')
+    p = 18 - (cy - y)
+    print('youll still enlist in {} years'.format(p))
+    
 
 elif cy - y == 18 :
     print('you must enlist in the army ')
 
 else :
-    print('you are late for enlistment ')
+    p2 = (cy - y) - 18
+    print('you are {} years late for enlistment '.format(p2))
