0f69587f1800c3220e529d732fc883fed01d0f64
sandersonfsl <sandersonfarias@gmail.com>
2022-06-09 17:11:53 -0300

    challenge52

--- challenges/challenge52.py
+++ challenges/challenge52.py
@@ -1,9 +1,15 @@
-n  = int(input('type an int number : '))
-
+print('I will read an int number and show you if it is a prime number ')
+print('-'*63)
+n = int(input('type an int number : '))
+s = 0
 for c in range (1,n+1):
-    a = [n/c]
+    if n % c == 0:
+        s = s + 1
 
-        
-
-
-
+if n != 1 :    
+    if s > 2 :
+        print('regular number ')
+    else :
+        print('prime number')
+else :
+    print('regular number ')
