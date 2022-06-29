cfddfc3bfba23fd66a6ea054eab371b1e62d5698
sandersonfsl <sandersonfarias@gmail.com>
2022-06-29 18:49:22 -0300

    challenge 59

--- challenges/challenge59(while sum mul biggest new number quit).py
+++ challenges/challenge59(while sum mul biggest new number quit).py
@@ -1,4 +1,42 @@
-c = 4 
+from time import sleep
+n1 = int(input(' n 1 = '))
+n2 = int(input(' n 2 = '))
+c = 0
+
+
+while c != 5 :
+
+    c = int(input(' [1] sum [2] mul [3] bigger [4] new numbers [5] quit \n '))
+
+    if c == 1 :
+        sum = n1 + n2 
+        print('sum : ',sum )
+    elif c == 2 :
+        mul = n1 * n2
+        print('mul ', mul)
+    elif c == 3 :
+        if n1 > n2 :
+            m = n1
+            print('bigger ', m)
+        else :
+            m = n2
+            print('bigger', n2)
+    elif c == 4 :
+        print('type 2 numbers again ')
+        n1 = int(input('n1 = '))
+        n2 = int(input('n2 = '))
+    elif c == 5 :
+        print('Exiting program ... ')
+        sleep(2)
+
+    else :
+        print('Invalid option ')
+
+print( ' Bye ! ')
+
+
+
+'''c = 4 
 while c == 4 :
     n1 = float(input(' number 1 : '))
     n2 = float(input(' number 2 : '))
@@ -17,4 +55,4 @@ while c == 3 :
     break
 while c == 5 :
     print('BYE !')
-    break
+    break '''
